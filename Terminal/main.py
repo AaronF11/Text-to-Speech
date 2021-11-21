a1ec7603bc081b825dd5b9561abad9ce64a6606f
Aaron Flores Pasos <aaron.flores7908@alumnos.udg.mx>
2021-11-20 19:47:32 -0600

    Documentar programa de la terminal

--- Terminal/main.py
+++ Terminal/main.py
@@ -8,16 +8,14 @@ from msvcrt import getch
 ESC = 27
 ENTER = 13
 
-def off():
+def off(): #Función para borrar y dar opción de salir | Function to delete and give option to exit
     system("cls")
     print("╔══════════════════════════════╗".center(127," "))
     print("║   [1] EXIT || [2] CONTINUE   ║".center(127," "))
     print("╚══════════════════════════════╝".center(127," "))
 
-                
-
-
-def title():
+def title(): #Función de titulo | Title function
+    print("\n\n\n\n\n")
     print("╔══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╗".center(127," "))
     print("║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║".center(127," "))
     print("║  ╠══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╣  ║".center(127," "))
@@ -32,7 +30,7 @@ def title():
     print("║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║".center(127," "))
     print("╚══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╝".center(127," "))
 
-def writeEnglish():
+def writeEnglish(): #Función para crear y sobrescribir txt en inglés | Function to create and overwrite txt in English
     file = open("EnglishFile.txt", "w")
     print(" WRITE TEXT ".center(120, "║"))
     record = str(input(""))
@@ -40,14 +38,14 @@ def writeEnglish():
     file.close()
     system("cls")
 
-def voiceEnglish():
+def voiceEnglish(): #Función para crear archivo de voz en inglés | Function to create voice file in Spanish
     with open("EnglishFile.txt", "r") as file:
         text = file.read()
     file = gTTS(text, lang = "en")
     filename = "English.mp3"
     file.save(filename)
 
-def writeSpanish():
+def writeSpanish(): #Función para crear y sobrescribir txt en español | Function to create and overwrite txt in Spanish
     file = open("SpanishFile.txt", "w")
     print(" WRITE TEXT ".center(120, "║"))
     record = str(input(""))
@@ -55,19 +53,18 @@ def writeSpanish():
     file.close()
     system("cls")
 
-def voiceSpanish():
+def voiceSpanish(): #Función para crear archivo de voz en español | Function to create voice file in Spanish
     with open("SpanishFile.txt", "r") as file:
         text = file.read()
     file = gTTS(text, lang = "es")
     filename = "Spanish.mp3"
     file.save(filename)
 
-def option():
-    print("\n")
+def option(): #Menu de opciones | Menu options
     print(" OPTION ".center(120, "║"))
     op = int(input(" -- > "))
     
-    #English version
+    #Versión Inglés | English version
     if op == 1:
         writeEnglish()
     elif op == 2:
@@ -75,7 +72,7 @@ def option():
         audio = "English.mp3"
         playsound(audio)
         remove("English.mp3")
-    #Spanish version
+    #Versión Español | Spanish version
     elif op == 3:
         writeSpanish()
     elif op == 4:
@@ -85,13 +82,13 @@ def option():
         remove("Spanish.mp3")
 
 
-def main():
+def main(): #Función main para mandar a llamar las demas funciones | Main function to call the other functions
     while True:
         title()
         option()
         off()
         print("".center(120, "║"))
-        op = int(input(" -- > "))
+        op = int(input(" -- > ")) #Opción para salir o continuar | Option to exit or continue
         if op == 1: 
             exit(0)
         if op == 2:
@@ -100,6 +97,3 @@ def main():
     
 if __name__ == '__main__':
     main()
-    
-
-
