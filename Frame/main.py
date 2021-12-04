55ab1b4e6d72e826228dc6e183fc32c6f0afdb7e
Aaron Flores Pasos <aaron.flores7908@alumnos.udg.mx>
2021-12-03 22:48:11 -0600

    Avanzar en el diseño del Frame

--- Frame/main.py
+++ Frame/main.py
@@ -1,41 +1,40 @@
 from logging import root
 from gtts import gTTS
 from playsound import playsound
-import tkinter
+from tkinter import *
 
-
-
-window = tkinter.Tk() #Crear la ventana
-window.geometry("400x200") #Dar tamaño
+window = Tk() #Crear la ventana
+window.title("Text-to-Speech") #Titulo de la ventana
+window.iconbitmap("src\ico\ico.ico") #Icono de la ventana
+#window.geometry("400x200") #Dar tamaño
 window.resizable(0,0) #Bloquear maximizar
 
-label = tkinter.Label(window, text = "Text to Speech", bg = "white", font = "console 20") #Crear la etiqueta
-label.pack(fill = tkinter.X) #Imprimir etiqueta
+frame = Frame()
+frame.pack()
+frame.config(bg="white", borderwidth=2, relief="ridge")
 
-def WriteText():
-    text = TextBox.get()
-    file = open("file.txt", "w")
-    file.write(text)
-    file.close()
-    with open("file.txt", "r") as file:
-        text = file.read()
-    file = gTTS(text=text,lang="ES")
-    filename = "voice.mp3"
-    file.save(filename)
-    audio = "voice.mp3"
-    playsound(audio)
+labelEnglish = Label(frame, text="ENGLISH", width=20, height=3, borderwidth=1, relief="sunken", bg="white", padx="3", pady="3")
+labelEnglish.grid(row=0, column=0)
+labelSpanish = Label(frame, text="SPANISH", width=20, height=3, borderwidth=1, relief="sunken", bg ="white", padx="3", pady="3")
+labelSpanish.grid(row=0, column=1)
 
+space = Label(frame, width=20)
+space.grid(row=1, column=0)
+space = Label(frame, width=20)
+space.grid(row=1, column=1)
 
-button = tkinter.Button(window, text = "Español", command= WriteText)
-button.pack()
+buttonWriteEnglish = Button(frame,text="WRITE", width=20, height=1, borderwidth=2, relief="raised", bg ="white", padx="1", pady="1")
+buttonWriteEnglish.grid(row=2, column=0)
+space = Label(frame, width=20)
+space.grid(row=3, column=0)
+buttonListenEnglish = Button(frame,text="LISTEN", width=20, height=1, borderwidth=2, relief="raised", bg ="white", padx="1", pady="1")
+buttonListenEnglish.grid(row=4, column=0)
 
-TextBox = tkinter.Entry(window)
-TextBox.pack()
+buttonWriteSpanish = Button(frame,text="WRITE", width=20, height=1, borderwidth=2, relief="raised", bg ="white", padx="1", pady="1")
+buttonWriteSpanish.grid(row=2, column=1)
+space = Label(frame, width=20)
+space.grid(row=3, column=1)
+buttonListenSpanish = Button(frame,text="LISTEN", width=20, height=1, borderwidth=2, relief="raised", bg ="white", padx="1", pady="1")
+buttonListenSpanish.grid(row=4, column=1)
 
-
-
-
-
-window.mainloop()
-
-    
+window.mainloop()    
